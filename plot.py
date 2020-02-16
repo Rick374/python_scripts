4a49c66b5076e38138514aea91760981f06b388e
Kaleid0S <ricki_lmassa@hotmail.com>
2020-02-16 04:20:55 -0300

    Update plot.py

--- plot.py
+++ plot.py
@@ -1,11 +1,18 @@
-import pylab as pl
+import sys
+import math
+from matplotlib import pyplot as pl
 
 def f1(x):
-    return x^2+1
+    return x**2+1
 
 def f2(x):
-    return x^3+3
+    return x**3+3
 
 x=range(-20,20)
 
+pl.figure("Función x^2+1")
 pl.plot(x, [f1(i) for i in x])
+pl.show()
+pl.figure("Función x^3+1")
+pl.plot(x, [f2(i) for i in x])
+pl.show()
